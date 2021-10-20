aa5907fd8d3eb0743e16ef33ba2da744d359af12
Prachi Gauriar <pgauriar@grubhub.com>
2021-10-19 22:03:39 -0400

    Improve output

--- xccovPretty.sh
+++ xccovPretty.sh
@@ -2,11 +2,7 @@
 
 XCCOVPRETTY="${GITHUB_ACTION_PATH}/xccovPretty"
 
-curl -L https://github.com/prachigauriar/xccovPretty/releases/download/1.0.0/xccovPretty -o "${XCCOVPRETTY}"
-
+curl -sSLo "${XCCOVPRETTY}" https://github.com/prachigauriar/xccovPretty/releases/download/1.0.0/xccovPretty
 chmod +x "${XCCOVPRETTY}"
-
-echo -e "\033[1;33m▸\033[0m Code Coverage"
-
 xcrun xccov view --report "${RESULT_BUNDLE}" --json 2> /dev/null \
     | "${XCCOVPRETTY}" -includeTargets "${INCLUDED_TARGETS}"
